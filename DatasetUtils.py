85cd4324306d26daf2493c8895a85f00efda7f69
ywcb00 <ywcb00@ywcb.org>
2026-03-06 17:08:35 +0100

    chore(DatasetUtils.py): load the respective dataset class dynamically

--- DatasetUtils.py
+++ DatasetUtils.py
@@ -1,7 +1,3 @@
-from tffdataset.FloodNetDataset import FloodNetDataset
-from tffdataset.IrisDataset import IrisDataset
-from tffdataset.MnistDataset import MnistDataset
-
 from enum import Enum
 
 class DatasetID(Enum):
@@ -12,10 +8,13 @@ class DatasetID(Enum):
 def getDataset(config):
     match config["dataset_id"]:
         case DatasetID.FloodNet:
+            from tffdataset.FloodNetDataset import FloodNetDataset
             return FloodNetDataset(config)
         case DatasetID.Mnist:
+            from tffdataset.MnistDataset import MnistDataset
             return MnistDataset(config)
         case DatasetID.Iris:
+            from tffdataset.IrisDataset import IrisDataset
             return IrisDataset(config)
         case _:
             raise NotImplementedError
@@ -23,10 +22,13 @@ def getDataset(config):
 def getDatasetElementSpec(config):
     match config["dataset_id"]:
         case DatasetID.FloodNet:
+            from tffdataset.FloodNetDataset import FloodNetDataset
             return FloodNetDataset.element_spec
         case DatasetID.Mnist:
+            from tffdataset.MnistDataset import MnistDataset
             return MnistDataset.element_spec
         case DatasetID.Iris:
+            from tffdataset.IrisDataset import IrisDataset
             return IrisDataset.element_spec
         case _:
             raise NotImplementedError
